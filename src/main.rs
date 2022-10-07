4e43d7d75e292aa6346a6d832d631e6db9117ab7
Oscar Henrichs <oscar.henrichs.work@outlook.com>
2022-10-07 01:54:16 -0300

    cors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate log;
+extern crate actix_web;
+extern crate serde_json;
+extern crate actix_cors;
+extern crate actix_rt;
+extern crate bcrypt;
+extern crate derive_more;
+extern crate dotenv;
+extern crate env_logger;
+extern crate failure;
+extern crate futures;
+extern crate jsonwebtoken;
+extern crate serde;
+extern crate uuid;
+
 mod routes;
 mod model;
 mod controller;
@@ -6,6 +22,8 @@ use routes::product::{ get_product, query_product, create_product};
  
 use mysql::*;
 use actix_web::*;
+use actix_cors::Cors;
+use actix_service::Service;
  
 #[actix_rt::main]
 async fn main() {
@@ -22,6 +40,12 @@ async fn main() {
  
     let server = match HttpServer::new(move || {
         App::new()
+            .wrap(
+            Cors::default() // allowed_origin return access-control-allow-origin: * by default
+                .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                .allowed_header(http::header::CONTENT_TYPE)
+                .max_age(3600),
+            )
             .app_data(shared_data.clone())
             .service(get_product)
             .service(query_product)
